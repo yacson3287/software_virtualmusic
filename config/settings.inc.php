0b674cbc64d520281d2ba9e56e2439f935c9d8bf
yacson <yacson@yacson.debian>
2016-04-02 13:18:19 -0430

    proyecto

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'virtual_music_DB');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '1234');
+define('_DB_PREFIX_', 'vm_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'qQUA4XJO0KaEm7cNQ1MVnpOmuMbU6v9EdJDsysF4SIiXFgqKnu95X8ei');
+define('_COOKIE_IV_', 'HXxNMXci');
+define('_PS_CREATION_DATE_', '2016-03-29');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.4');
+define('_RIJNDAEL_KEY_', 'bNyWKBT8vdAfGE7uinEEQSaFAc5eAF6p');
+define('_RIJNDAEL_IV_', 'BOjCVc3v5l2ZdQZ9qcxc7g==');
